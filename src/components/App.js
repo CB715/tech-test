9d3c3bfce7e3db1460f1370d028f9d207186309a
Carl Beckley <carlbeckley@gmail.com>
2023-02-04 11:40:45 +0000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/app.css';
+
+import Search from '../components/Search'
+
+const App = () => {
+  return (
+    <div className="App">
+      <img 
+        className='nasa-logo' 
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" 
+        alt='NASA Logo'/>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import react, { useState } from 'react';
+import '../styles/search.css';
+
+const Search = () => {
+    return (
+    <>
+    <input className='search-box' type='text' placeholder='Search' />
+    </> 
+    );
+};
+
+export default Search;
