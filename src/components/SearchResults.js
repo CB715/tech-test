957273572953f3fad80f4515d92fb0777c7cc3ba
Carl Beckley <carlbeckley@gmail.com>
2023-02-04 20:16:38 +0000

    placeholder image added

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,13 @@
 import React from "react";
+import '../styles/searchResults.css'
 
 const SearchResults = () => {
     return (
-        <div className='search-results'>
+      <div className='search-results'>
         <p>Search Results</p>
-        </div>
+        <img src='https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80' 
+        alt='placeholder'/>
+      </div>
     )
 };
 
