f8440b71888dc95bae703407aa6974f8a295879f
Carl Beckley <carlbeckley@gmail.com>
2023-02-04 12:35:01 +0000

    added search button to search component and added useState to input

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,22 @@ import react, { useState } from 'react';
 import '../styles/search.css';
 
 const Search = () => {
+    const [value, setValue] = useState();
     return (
     <>
-    <input className='search-box' type='text' placeholder='Search' />
+    <form className='search-form'>
+    
+        <input 
+            className='search-form__input-box' 
+            type='text' 
+            placeholder='Search' 
+            onChange={(e) => setValue(e.target.value)}
+        />
+    
+        <button className='search-form__button' type='submit'>
+            Search
+        </button>
+    </form>
     </> 
     );
 };
