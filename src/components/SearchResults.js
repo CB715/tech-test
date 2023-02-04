46b69fa861c75b2a9fba15fd35576faabb3d68e3
Carl Beckley <carlbeckley@gmail.com>
2023-02-04 20:08:43 +0000

    created initial component to display search results

---
+++ src/components/SearchResults.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchResults = () => {
+    return (
+        <div className='search-results'>
+        <p>Search Results</p>
+        </div>
+    )
+};
+
+export default SearchResults
