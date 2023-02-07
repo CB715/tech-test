7b39b04edbf8a6094c90c5cbb99205bd680c363d
Carl Beckley <carlbeckley@gmail.com>
2023-02-07 11:57:10 +0000

    added some basic styling to align the returned images

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,14 +4,18 @@ import '../styles/searchResults.css'
 
 const SearchResults = ({ results }) => {
   if (!results.length) {
-    return <p>No results</p>
+    return <p className='search-results__no-results'>No results</p>
   } else {
     return (
       <div className='search-results'>
-        <p>Search Results:</p>
+        <div className='search-results__text'>
+          <p>Search Results:</p>
+        </div>
+        <div className='search-results__images'>
         {results.map((image) => (
           <img className="card-image" src={image} alt="spaceImage" />
-        ))};
+        ))}
+        </div>
       </div>
     )
   };
