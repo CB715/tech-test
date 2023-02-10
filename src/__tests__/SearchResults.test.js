298e01238483da32b98fdf593ea505c9f44e05ac
Carl Beckley <carlbeckley@gmail.com>
2023-02-10 13:53:54 +0000

    amended imports. Incorrect component was used

--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react'
-import SearchResults from "../components/Search";
+import SearchResults from '../components/SearchResults';
 
 describe('Search Results', () => {
   const { asFragment } = render(<SearchResults />);
