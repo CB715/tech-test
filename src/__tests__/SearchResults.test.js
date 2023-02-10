5b48c211079bebcd577a140bf33fb171e03eb3d4
Carl Beckley <carlbeckley@gmail.com>
2023-02-10 14:39:13 +0000

    added validProps to test

--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -3,9 +3,12 @@ import { render } from '@testing-library/react'
 import SearchResults from '../components/SearchResults';
 
 describe('Search Results', () => {
-  const { asFragment } = render(<SearchResults />);
+  const validProps = {
+    results: ['string1', 'string2', 'string3']
+  };
+  const { asFragment } = render(<SearchResults results={validProps.results}/>);
 
-    it('renders correctly', () => {
-      expect(asFragment()).toMatchSnapshot();
+  it('renders correctly', () => {
+    expect(asFragment()).toMatchSnapshot();
   });
 });
